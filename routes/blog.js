5f911712590e3e49721787438b26aedd2fc3ac3b
mrjvs <jellevs@gmail.com>
2018-10-01 15:55:49 +0200

    blog post display and loading

---
+++ routes/blog.js
@@ -0,0 +1,33 @@
+/*
+
+blog.js -
+file for handling routes regarding blog posts.
+
+*/
+
+// import express' router
+const router = require('express').Router();
+const blogHelper = require("../helpers/blog-helper.js");
+
+// display blog post
+router.get('/news/:id', async (req, res) => {
+
+    if (isNaN(req.params.id)) {
+        res.statusCode = 404;
+        res.render("404");
+        return;
+    }
+
+    const hbsObject = await blogHelper.getBlogPostExpressReady(req.params.id);
+
+    if (!hbsObject) {
+        res.statusCode = 404;
+        res.render("404");
+        return;
+    }
+
+	res.render('post', hbsObject);
+});
+
+// export the router
+module.exports = router;
