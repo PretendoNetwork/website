26058d8fa98f98242f352bd39bdc8202e6c44cf2
RedDuckss <jonbarrow1998@gmail.com>
2018-10-11 04:09:13 -0400

    Added linting
    Added the linter ESLint, added a custom set of rules and fixed linting issues

--- server.js
+++ server.js
@@ -7,27 +7,27 @@ the file that contains the code to run the server
 
 // module imports, and
 // some important variable defs
-const express = require("express"),
-	handlebars = require("express-handlebars"),
-	session = require("express-session"),
-	passport = require("passport"),
-	LocalStrategy = require("passport").Strategy,
-	bcrypt = require("bcrypt"),
-	mongoose = require("mongoose"),
-	bodyParser = require("body-parser");
-	config = require("./config.json"),
-	app = express(),
-	common = require('./helpers/common'),
-	adminUserModel = require("./models/admin-user").adminUserModel;
+const express = require('express');
+const handlebars = require('express-handlebars');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport').Strategy;
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const config = require('./config.json');
+const app = express();
+const common = require('./helpers/common');
+const adminUserModel = require('./models/admin-user').adminUserModel;
 
 // import the colors module
-require("colors");
+require('colors');
 
 // locations for some files
 const locations = {
-	home: require("./routes/home"),
-	posts: require("./routes/blog"),
-	admin: require("./routes/admin")
+	home: require('./routes/home'),
+	posts: require('./routes/blog'),
+	admin: require('./routes/admin')
 };
 
 // setup body parser
@@ -36,18 +36,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // setup database
 mongoose.connect(config.database.url);
 const connection = mongoose.connection;
-connection.on("error", console.error.bind(console, "connection error:"));
+connection.on('error', console.error.bind(console, 'connection error:'));
 
 // setup session
 app.use(session({
 	secret: config.secrets.session,
-  	/*cookie: { secure: true } // use when https is enabled*/
+	//cookie: { secure: true } // use when https is enabled
 }));
 
 // setup authentication
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use("adminUserStrategy", new LocalStrategy(
+passport.use('adminUserStrategy', new LocalStrategy(
 	(username, password, done) => {
 		// find user in database
 		adminUserModel.findByUsername(username).then((user) => {
@@ -76,20 +76,20 @@ passport.use("adminUserStrategy", new LocalStrategy(
 ));
 
 // load the handlebars module
-app.engine(".hbs", handlebars({
-        extname: ".hbs",
-        layoutsDir: "views",
-		partialsDir: "views/partials"
+app.engine('.hbs', handlebars({
+	extname: '.hbs',
+	layoutsDir: 'views',
+	partialsDir: 'views/partials'
 }));
 
 // set express to use handlebars
 // as the templating engine
-app.set("view engine", ".hbs");
+app.set('view engine', '.hbs');
 
 // set some routes on the server
 
 // assets folder serving
-app.use("/assets", express.static("assets"));
+app.use('/assets', express.static('assets'));
 
 // website root
 app.use('/', locations.home);
@@ -106,5 +106,5 @@ app.use(common.sendDefault404);
 
 // start the server
 app.listen(config.http.port, () => {
-	console.log(`started the server on port: ${ new String(config.http.port).green }`);
+	console.log(`started the server on port: ${new String(config.http.port).green}`);
 });
