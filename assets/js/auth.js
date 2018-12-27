d519b4ae0e3532ac6f00d8c9e3331e118e1744a8
mrjvs <jellevs@gmail.com>
2018-12-27 14:10:35 +0100

    made auth forms usuable for end user

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -11,16 +11,44 @@ function postAjax(url, data, success) {
 	return xhr;
 }
 
+function showErrorPopup(errorText) {
+	console.log(errorText)
+}
+
 function login() {
 	var xhr = postAjax('/api/v1/login', {
 		email: document.getElementById('email_input').value,
 		password: document.getElementById('password_input').value
 	});
 	xhr.onload = function () {
-		console.log(xhr.responseText);
+		if (xhr.responseText.charAt(0) !== "{") {
+			return showErrorPopup('Whoops, something went wrong. Try again later.');
+		}
 		response = JSON.parse(xhr.responseText);
 		if (response.success === true) {
 			location.pathname = '/pnid/dashboard';
+		} else if (response.errors) {
+			showErrorPopup(response.errors);
+		}
+	}
+}
+
+function register() {
+	var xhr = postAjax('/api/v1/register', {
+		email: document.getElementById('email_input').value,
+		username: document.getElementById('username_input').value,
+		password: document.getElementById('password_input').value,
+		confirm_password: document.getElementById('password_confirm_input').value
+	});
+	xhr.onload = function () {
+		if (xhr.responseText.charAt(0) !== "{") {
+			return showErrorPopup('Whoops, something went wrong. Try again later.');
+		}
+		response = JSON.parse(xhr.responseText);
+		if (response.success === true) {
+			login();
+		} else if (response.errors) {
+			showErrorPopup(response.errors);
 		}
 	}
 }
