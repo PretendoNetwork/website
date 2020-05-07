00102ee7384033f35465e253ab3b0bc2e6bf71dc
Jip Fr <jipfrijlink@gmail.com>
2020-05-07 12:13:58 +0200

    Locale business

---
+++ util/getLocale.js
@@ -0,0 +1,8 @@
+module.exports = (variant, language) => {
+	try {
+		return require(`../locales/${variant}_${language}.json`);
+	} catch(e) {
+		// Use US_en as a fallback
+		return require("../locales/US_en.json");
+	}
+}
