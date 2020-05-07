9cb7635ad5eaf97db021da8fa2da643b2987a8bd
Jip Fr <jipfrijlink@gmail.com>
2020-05-07 16:08:07 +0200

    Correct FAQ layout

--- web-server/index.js
+++ web-server/index.js
@@ -11,7 +11,32 @@ const routers = {
 
 app.use("/", routers.home);
 
-app.engine("handlebars", handlebars());
+app.engine("handlebars", handlebars({
+	helpers: {
+		doFaq(value, options) {
+			let htmlLeft = "";
+			let htmlRight = "";
+			for(let [i, v] of Object.entries(value)) {
+				let appendHtml = options.fn({
+					...v
+				}); // Tis is an HTML string
+				if(i % 2 === 0) {
+					htmlLeft += appendHtml;
+				} else {
+					htmlRight += appendHtml;
+				}
+			}
+			return `
+			<div class="left questions-left">
+				${htmlLeft}
+			</div>
+			<div class="right questions-right">
+				${htmlRight}
+			</div>
+			`;
+		}
+	}
+}));
 app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
