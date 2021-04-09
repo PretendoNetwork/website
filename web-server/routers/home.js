754b9be7895a28d6f3260a85f36e8dbce7bb03e6
Jip Fr <jipfrijlink@gmail.com>
2021-04-09 15:10:12 +0200

    Import lists from json

--- web-server/routers/home.js
+++ web-server/routers/home.js
@@ -2,24 +2,14 @@ const getLocale = require('../../util/getLocale');
 const { Router } = require('express');
 const router = new Router();
 
+const featureLists = require('../../feature-lists.json');
+
 router.get('/', (req, res) => {
 	const tmpLocale = getLocale('US', 'en');
 	res.render('home', {
 		layout: 'main',
 		locale: tmpLocale,
-		featuredFeatureList: {
-			title: 'Thing',
-			features: [
-				{
-					name: 'Feature A', 
-					checked: true
-				},
-				{
-					name: 'Feature B', 
-					checked: false
-				}
-			]
-		}
+		featuredFeatureList: featureLists[0]
 	});
 });
 
