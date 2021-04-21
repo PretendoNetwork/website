f248aeed1ca8e20dcf44c9a1446a8ee448b1f7e8
Jip Fr <jipfrijlink@gmail.com>
2021-04-21 10:51:12 +0200

    Oops — fix logger

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const routers = {
 };
 
 app.use('*', (req, res, next) => {
-	logger.info(`${req.method.toUpperCase()} ${req.path}${req.filepath}`);
+	logger.info(`${req.method.toUpperCase()} ${req.originalUrl}`);
 	next();
 });
 
