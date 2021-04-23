481b05121383f943342b697cbed4e751c060d9f9
Jonathan Barrow <jonbarrow1998@gmail.com>
2021-04-23 18:06:47 -0400

    moved checkboxes out of JS
    checkbox code now lives entirely in the handlebars views and not inside the JS

--- src/index.js
+++ src/index.js
@@ -44,24 +44,6 @@ app.engine('handlebars', handlebars({
 				${htmlRight}
 			</div>
 			`;
-		},
-		customCheckbox(type) {
-			switch(type) {
-				case 'completed':
-					return `
-						<div class="custom-checkbox done">
-							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check"><polyline points="20 6 9 17 4 12"></polyline></svg>
-						</div>
-					`;
-				case 'started':
-					return `
-						<div class="custom-checkbox ongoing">
-							<div class="small-dot"></div>
-						</div>
-					`;
-				default:
-					return '<div class="custom-checkbox incomplete"></div>';
-			}
 		}
 	}
 }));
