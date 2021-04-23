7ed5b81a13e4b466ba351e3277f5852b9d8e1280
Jonathan Barrow <jonbarrow1998@gmail.com>
2021-04-23 19:51:43 -0400

    Moved getLocale to util.js

--- src/util.js
+++ src/util.js
@@ -1,7 +1,23 @@
+const fs = require('fs-extra');
+const logger = require('./logger');
+
 function fullUrl(request) {
 	return `${request.protocol}://${request.hostname}${request.originalUrl}`;
 }
 
+function getLocale(region, language) {
+	const path = `${__dirname}/../locales/${region}_${language}.json`;
+
+	if (fs.pathExistsSync(path)) {
+		return require(path);
+	}
+
+	logger.warn(`Could not find locale ${region}_${language}! Loading US_en`);
+
+	return require(`${__dirname}/../locales/US_en.json`);
+}
+
 module.exports = {
-	fullUrl
+	fullUrl,
+	getLocale
 }
--- util/getLocale.js
+++
@@ -1,8 +0,0 @@
-module.exports = (variant, language) => {
-	try {
-		return require(`../locales/${variant}_${language}.json`);
-	} catch(e) {
-		// Use US_en as a fallback
-		return require('../locales/US_en.json');
-	}
-};
