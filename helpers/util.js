a6dc386cff7e6cd1f16f7a0dd286fc7821f12579
Carlos Nuñez <carlitosnu41@gmail.com>
2021-04-23 22:02:30 -0500

    Add Spanish lang and Auto lang setter (2nd Attemp)

--- helpers/util.js
+++ helpers/util.js
@@ -8,19 +8,19 @@ small commonly used utilities
 const fs = require('fs-extra');
 const logger = require('../logger');
 
-let setLocales = (request)=> {
-	return request.headers['accept-language'].split("-")[0].toUpperCase();
-}
+const setLocales = (request)=> {
+	return request.headers['accept-language'].split('-')[0].toUpperCase();
+};
 // Returns a locale
 function getLocale(request) {
-	let locale = setLocales(request)
+	const locale = setLocales(request);
 	const path = `${__dirname}/../locales/${locale}.json`;
 
 	if (fs.pathExistsSync(path)) {
 		return require(path);
 	}
 
-	logger.warn(`Could not find locale ${region}_${language}! Loading default`);
+	logger.warn(`Could not find locale ${locale}! Loading default`);
 
 	return getDefaultLocale();
 }
