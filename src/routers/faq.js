cd22e72f7be9dbd36a122070512d28f4ddff1fd8
Monty <monty.github@gmail.com>
2021-05-26 15:12:22 +0200

    Fix broken /faq redirect

--- src/routers/faq.js
+++ src/routers/faq.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = new Router();
 
 router.get('/', (request, response) => {
-	res.redirect('/#faq');
+	response.redirect('/#faq');
 });
 
 module.exports = router;
