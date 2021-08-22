84b1e09bc631363e61ede11dd8cfac3c0ab04cd2
montylion <hey@montylion.dev>
2021-08-22 18:56:54 +0200

    Update function names for consistency

--- public/assets/js/locale-dropdown-handler.js
+++ public/assets/js/locale-dropdown-handler.js
@@ -1,10 +1,10 @@
-function setFormDefaultLocale(localeString) {
+function setDefaultDropdownLocale(localeString) {
 	try {
 		document.querySelector(`option[value=${localeString}`).selected = true;
 	} catch {} // If the locale isn't listed
 }
 
-function localeFormHandler() {
+function localeDropdownHandler() {
 	const selectedLocale = document.querySelector("#locale-dropdown").value;
 	document.cookie = `preferredLocale=${selectedLocale};max-age=31536000`;
 	window.location.reload();
