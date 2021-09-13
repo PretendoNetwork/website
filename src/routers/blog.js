852b1de7c94df02381e8c2f1b37241c08bbc2589
Jip Fr <jipfrijlink@gmail.com>
2021-09-13 15:33:46 +0200

    feat(blog): add hard-coded blog page w/ base design

--- src/routers/blog.js
+++ src/routers/blog.js
@@ -1,9 +1,7 @@
 const { Router } = require('express');
 const util = require('../util');
-const { boards } = require('../../boards/boards.json');
 const router = new Router();
 
-const { getTrelloCache } = require('../trello');
 
 router.get('/', async (request, response) => {
 
@@ -12,14 +10,10 @@ router.get('/', async (request, response) => {
 
 	const localeString = reqLocale.toString();
 
-	const cache = await getTrelloCache();
-
-	response.render('progress', {
+	response.render('blog', {
 		layout: 'main',
-		boards,
 		locale,
 		localeString,
-		progressLists: cache
 	});
 });
 
