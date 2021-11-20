4079e3f99a5bac6f475e5b1cf16a829220a60384
Jonathan Barrow <jonbarrow1998@gmail.com>
2021-11-20 17:41:17 -0500

    Discord account linking

--- src/util.js
+++ src/util.js
@@ -1,4 +1,5 @@
 const fs = require('fs-extra');
+const got = require('got');
 const logger = require('./logger');
 
 function fullUrl(request) {
@@ -17,7 +18,44 @@ function getLocale(region, language) {
 	return require(`${__dirname}/../locales/US_en.json`);
 }
 
+function apiGetRequest(path, headers) {
+	return got.get(`https://api.pretendo.cc${path}`, {
+		responseType: 'json',
+		throwHttpErrors: false,
+		https: {
+			rejectUnauthorized: false, // Needed for self-signed certificates on localhost testing
+		},
+		headers
+	});
+}
+
+function apiPostGetRequest(path, headers, json) {
+	return got.post(`https://api.pretendo.cc${path}`, {
+		responseType: 'json',
+		throwHttpErrors: false,
+		https: {
+			rejectUnauthorized: false, // Needed for self-signed certificates on localhost testing
+		},
+		headers,
+		json
+	});
+}
+
+function apiDeleteGetRequest(path, headers, json) {
+	return got.delete(`https://api.pretendo.cc${path}`, {
+		throwHttpErrors: false,
+		https: {
+			rejectUnauthorized: false, // Needed for self-signed certificates on localhost testing
+		},
+		headers,
+		json
+	});
+}
+
 module.exports = {
 	fullUrl,
-	getLocale
+	getLocale,
+	apiGetRequest,
+	apiPostGetRequest,
+	apiDeleteGetRequest
 };
