26c8bec57efdd55b105f038d2e0512fca1935a36
Eyadplayz <Eyadplayz@users.noreply.github.com>
2022-02-02 22:47:13 +0200

    Fixed Mobile Section not working on Search
    I think?

--- public/assets/js/docs-mobile-handler.js
+++ public/assets/js/docs-mobile-handler.js
@@ -4,7 +4,9 @@ button.addEventListener('click',function() {
     sidebar.style.visibility = "visible"
     sidebar.style.width = "60mm"
 })
-document.getElementsByClassName('content')[0].addEventListener('click', (e) => {
-    sidebar.style.visibility = "hidden"
-    sidebar.style.width = "0mm"
+document.addEventListener('click', (e) => {
+    if (!sidebar.contains(e.target) && !button.contains(e.target)) {
+        sidebar.style.visibility = "hidden"
+        sidebar.style.width = "0mm"   
+    }
 })
