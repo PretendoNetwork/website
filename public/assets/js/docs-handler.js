a609c6396a9a77840a60001e4d6feec47a5ac4ea
Eyadplayz <Eyadplayz@users.noreply.github.com>
2022-02-19 02:00:04 +0200

    Some Fixes and Tweeks

--- public/assets/js/docs-handler.js
+++ public/assets/js/docs-handler.js
@@ -27,4 +27,15 @@ function selectSidebarElement(element) {
   let aURL = location.pathname.split("/")
   let hrefURL = aURL[2]
   if (hrefURL.includes("errorcodes")) {hrefURL += `/${aURL[3]}`}
+  if (document.querySelector(`div.sidebar a[href='/docs/${hrefURL}']`)) {
   selectSidebarElement(document.querySelector(`div.sidebar a[href='/docs/${hrefURL}']`));
+  }else{
+      //Assumes that Search and the Docs with Error Codes Mixed Together so this *tries* to find it
+    let elems = document.body.getElementsByClassName("section");
+    for (let section = 0;section < elems.length;section++){
+    for (let eleValue = 0;eleValue < elems[section].children.length;eleValue++){
+     if (elems[section].children[eleValue].textContent.includes(aURL[3])){
+        selectSidebarElement(elems[section].children[eleValue]);break;}
+    }
+  }
+}
