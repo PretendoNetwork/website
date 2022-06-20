ad60c7083173766862b46669593fa5c38db1e692
Jonathan Barrow <jonbarrow1998@gmail.com>
2022-06-20 17:16:28 -0400

    Use a full Mongo URI instead of address and port in config

--- src/database.js
+++ src/database.js
@@ -3,12 +3,11 @@ const PNIDSchema = require('./schema/pnid');
 const config = require('../config.json');
 
 const accountServerConfig = config.database.account;
-const accountServerURI = `mongodb://${accountServerConfig.address}:${accountServerConfig.port}/${accountServerConfig.database_name}`;
 let accountServerDBConnection;
 let PNID;
 
 async function connect() {
-	accountServerDBConnection = await mongoose.createConnection(accountServerURI, accountServerConfig.options);
+	accountServerDBConnection = await mongoose.createConnection(accountServerConfig.uri, accountServerConfig.options);
 	accountServerDBConnection.on('error', console.error.bind(console, 'Mongoose connection error:'));
 	accountServerDBConnection.on('close', () => {
 		accountServerDBConnection.removeAllListeners();
