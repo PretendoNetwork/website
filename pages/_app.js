96661adc761db98db31872a891e9721e81c44b51
Ash Monty <git@ashmonty.com>
2022-11-27 12:32:19 +0100

    feat: set up font optimization

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css';
 
+import { Poppins } from '@next/font/google';
+const poppins = Poppins({
+	weight: ['400', '700'],
+	fallback: ['Arial', 'Helvetica', 'system-ui', 'sans-serif'],
+	subsets: ['latin'], // this is the preloaded subset, all subsets will be available
+});
+
 function Pretendo({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<main style={poppins.style}>
+			<style jsx global>{`
+				:root {
+					--font-family: ${poppins.style.fontFamily};
+				}
+			`}</style>
+			<Component {...pageProps} />
+		</main>
+	);
 }
 
 export default Pretendo;
