8181b131191a4a3a2fe5d057263965aa4cf69e29
Ash Monty <git@ashmonty.com>
2023-04-01 00:10:23 +0200

    feat(component): style button component to support icon-only buttons

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -6,6 +6,7 @@ import styles from './Button.module.css';
  * A reusable component for buttons.
  *
  * @param {boolean} isPrimary - Whether the button is primary or not. Defaults to false.
+ * @param {boolean} isIcon - Whether the button only contains an icon. Defaults to false.
  * @param className - An optional classname.
  * @param {string} style - Custom styles to apply to the title.
  *
@@ -15,9 +16,9 @@ import styles from './Button.module.css';
  */
 
 export default function Button(ctx) {
-	const { children, className, isPrimary, onClick } = ctx;
+	const { children, className, isPrimary, isIcon, onClick } = ctx;
 	return (
-		<button className={classNames(styles.button, { [styles.primary]: isPrimary }, className)} onClick={onClick}>
+		<button className={classNames(styles.button, { [styles.primary]: isPrimary, [styles.icon]: isIcon }, className)} onClick={onClick}>
 			{children}
 		</button>
 	);
