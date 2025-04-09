a599c1c6a6aaeb35ace8b258d16b440ac751133b
mrjvs <mistrjvs@gmail.com>
2025-04-09 13:04:36 +0200

    feat: add basic nuxt app

---
+++ nuxt-app/nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/content'
+  ]
+})
