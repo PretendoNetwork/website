7ee78ad7dac446f14dd33cb738076c44c2496c0b
William Oldham <github@binaryoverload.co.uk>
2025-04-14 18:35:07 +0100

    style: add typescript parser for vue files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,15 @@ import eslintConfig from '@pretendonetwork/eslint-config';
 import { withNuxt } from './.nuxt/eslint.config.mjs';
 
 export default withNuxt([
-	...pluginVue.configs['flat/recommended'],
 	...eslintConfig,
+	...pluginVue.configs['flat/recommended'],
 	{
+		files: ['*.vue', '**/*.vue'],
+		languageOptions: {
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		},
 		rules: {
 			'vue/multi-word-component-names': 'off'
 		}
