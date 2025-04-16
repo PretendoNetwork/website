6d89c5632fe2e72cefe60b09efe1d7639e47cf10
limes.pink <git@limes.pink>
2025-04-16 18:25:07 +0200

    fix(blog): implement feedback by jvs

--- src/components/MdIframe/MdIframe.vue
+++ src/components/MdIframe/MdIframe.vue
@@ -1,13 +1,14 @@
 <script lang="ts" setup>
-// eslint-disable-next-line vue/prop-name-casing -- this is gonna be used in markdown
-const { videoId } = defineProps({ 'video-id': { type: String, required: true } });
+const props = defineProps<{
+	videoId: string;
+}>();
 </script>
 
 <template>
   <iframe
     width="560"
     height="315"
-    :src="`https://www.youtube-nocookie.com/embed/${videoId}`"
+    :src="`https://www.youtube-nocookie.com/embed/${props.videoId}`"
     title="YouTube video player"
     frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
