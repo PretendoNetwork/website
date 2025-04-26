26817d5fbc9654f1068afc419a8f2d100ca34ec9
niko <57009359+hauntii@users.noreply.github.com>
2025-04-25 21:26:27 -0400

    feat: registration and css separation

--- src/server/api/account/register/index.ts
+++ src/server/api/account/register/index.ts
@@ -1,1 +1,36 @@
-// TODO: registration
+import { FetchError } from 'ofetch';
+
+// TODO: Drop to interface from type, type is unnecessary for this
+type RegisterCCResponse = { // TODO: this is the same as the login response. figure out where we should put types and merge into AuthCCReponse!
+	refresh_token: string;
+	access_token: string;
+	token_type: string;
+	expires_in: number;
+};
+
+export default defineEventHandler(async (event) => {
+	const body = await readBody(event);
+
+	try {
+		const apiResponse = await $fetch<RegisterCCResponse>(`/v1/register`, {
+			method: 'POST',
+			baseURL: useRuntimeConfig(event).apiBase,
+			body: body
+		});
+
+		setCookie(event, 'refresh_token', apiResponse.refresh_token);
+		setCookie(event, 'access_token', apiResponse.access_token);
+		setCookie(event, 'token_type', apiResponse.token_type);
+
+		setResponseStatus(event, 200);
+		event.node.res.end();
+	} catch (error: unknown) {
+		if (error instanceof FetchError) {
+			setResponseStatus(event, error.status, error.data.error);
+		} else {
+			setResponseStatus(event, 500);
+		}
+
+		event.node.res.end();
+	}
+});
